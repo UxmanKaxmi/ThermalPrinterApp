92b9f0b0d58ea577dcc374c46a8f088a7c68f139
Uxman <ZAKI_US94@HOTMAIL.COM>
2018-10-12 17:33:36 +0500

    asd
    asd

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
+import { PrinterListModalPage } from '../pages/printer-list-modal/printer-list-modal';
+import { PrintProvider } from '../providers/print/print';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    PrinterListModalPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    PrinterListModalPage
+  ],
+  providers: [
+    StatusBar,
+    BluetoothSerial,
+    SplashScreen,
+    PrintProvider,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
